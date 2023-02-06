e27dd693e402f92bd7947db99209c7098d0580da
topujss <adahmedt@gmail.com>
2023-02-06 17:29:10 -0500

    project ongoing

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,23 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Im Home</h1>
-    </>
-  );
+import { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <main>
+          <div aria-label="card-item-v1" className="flex flex-col w-[400px]">
+            <div className="relative flex-shrink-0 mb-5 h-[250px]">
+              <img src="https://bit.ly/3zzCTUT" alt="" className="object-cover w-full h-full rounded-lg" />
+            </div>
+            <div className="flex flex-col flex-1">
+              <h3 className="mb-3 text-lg font-bold">Welcome to the best place to travel on the world</h3>
+              <div className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </div>
+          </div>
+        </main>
+      </>
+    );
+  }
 }
+
+export default Home;
